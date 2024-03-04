d0315a07a309a1d1e8b3f6a389b3b26af9c41c47
aaronnossek <91189204+aaronnossek@users.noreply.github.com>
2024-03-04 11:16:03 +0100

    Create credits.js

---
+++ credits.js
@@ -0,0 +1,5 @@
+credits();
+function credits() {
+    var credits = '----------\n\nThis website was designed and built by Aaron Nossek.\nwww.aaronnossek.com\n\n----------';
+    console.log('%c' + credits, 'color: #00F; font-family: Helvetica Neue", Helvetica, sans-serif; font-size: 14px;');	
+}
